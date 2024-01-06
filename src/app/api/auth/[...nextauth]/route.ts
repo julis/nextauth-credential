a6e2384c2548597e47770d02101638a0e5039e87
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-05 18:33:54 -0600

    Refactor NextAuth route handlers

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,15 @@
 // /src/app/api/auth/[...nextauth]/route.ts
 
-import NextAuth from "next-auth";
+import { NextApiRequest, NextApiResponse } from 'next';
+import NextAuth from 'next-auth';
+import { authOptions } from '@/server/auth';
 
-import { authOptions } from "@/server/auth";
+const handleGet = (req: NextApiRequest, res: NextApiResponse) => {
+  return NextAuth(req, res, authOptions);
+};
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+const handlePost = (req: NextApiRequest, res: NextApiResponse) => {
+  return NextAuth(req, res, authOptions);
+};
+
+export { handleGet as GET, handlePost as POST };
